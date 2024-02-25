58463421b05e7522c0dd2f2c891dc8d8c6f7dc3c
Reezzz702 <changhowad@gmail.com>
2024-02-25 20:36:36 +0800

    eval easy

--- eval_config/generate_scenarios.py
+++ eval_config/generate_scenarios.py
@@ -2,9 +2,9 @@ import json
 from copy import deepcopy
 
 agents = ["auto", "plant", "roach", "sensor"]
-eavl_agent = "auto"
+eavl_agent = "sensor"
 
-f = open("1to1/eval_easy.json")
+f = open("eval_easy.json")
 eval_config = json.load(f)
 scenario_list = []
 index = 0
@@ -19,15 +19,15 @@ for scenario in eval_config['available_scenarios']:
     scenario_list.append(temp_scenario)
     if agent == eavl_agent:
       continue
-    else:
-      scenario['ego_agent']['type'] = agent
-      scenario['Index'] = index
-      index += 1
-      for other_agent in scenario['other_agents']:      
-        other_agent['type'] = eavl_agent
-      temp_scenario = deepcopy(scenario)
-      scenario_list.append(temp_scenario)
+
+    scenario['ego_agent']['type'] = agent
+    scenario['Index'] = index
+    index += 1
+    for other_agent in scenario['other_agents']:      
+      other_agent['type'] = eavl_agent
+    temp_scenario = deepcopy(scenario)
+    scenario_list.append(temp_scenario)
     
 eval_config['available_scenarios'] = scenario_list
-with open("1to1/auto_eval_easy.json", 'w') as fd:
+with open(f"{eavl_agent}_eval_easy.json", 'w') as fd:
   json.dump(eval_config, fd, indent=2, sort_keys=False)
