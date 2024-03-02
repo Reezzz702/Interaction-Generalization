f2c1f5deb546514ae73e272e5603403d4b684c09
tonybear27 <tony20011027@gmail.com>
2024-03-02 19:09:36 +0800

    local

--- run_eval_local.sh
+++ run_eval_local.sh
@@ -9,27 +9,29 @@ export PYTHONPATH=$PYTHONPATH:${WORK_DIR}/team_code
 
 export EVAL_CONFIG=${WORK_DIR}/eval_config/sensor_eval_easy.json
 export SENSOR_AGNET=${WORK_DIR}/SRL_agent
-export AGENT_CONFIG=${WORK_DIR}/checkpoints/BB_DS_wp
+export AGENT_CONFIG=${WORK_DIR}/checkpoints/BB_BS_R_wp
 export DIRECT=0
 export RESUME=1
-export CHECKPOINT_ENDPOINT=${WORK_DIR}/results/easy/BB_DS/interaction.json
-export SAVE_PATH=${WORK_DIR}/results/easy/BB_DS
+export REND=0
+export CHECKPOINT_ENDPOINT=${WORK_DIR}/results/easy/BB_BS_R/interaction.json
+export SAVE_PATH=${WORK_DIR}/results/easy/BB_BS_R
+export CARLA_PORT=3000
+#
+# killall -9 -r CarlaUE4-Linux 
+# bash ${CARLA_ROOT}/CarlaUE4.sh -RenderOffScreen -carla-rpc-port=${CARLA_PORT} &
 
-
-killall -9 -r CarlaUE4-Linux 
-bash ${CARLA_ROOT}/CarlaUE4.sh &
-
-sleep 5
+# sleep 5
 
 python multi_agent_eval.py \
+--port ${CARLA_PORT} \
 --eval_config ${EVAL_CONFIG} \
 --sensor_agent ${SENSOR_AGNET} \
 --agent_config ${AGENT_CONFIG} \
 --checkpoint ${CHECKPOINT_ENDPOINT} \
 --resume ${RESUME}
 
-sleep 5
+# sleep 5
 
-killall -9 -r CarlaUE4-Linux 
+# killall -9 -r CarlaUE4-Linux 
 
-sleep 1
+# sleep 1
